7660423e0ed1c42869e5a1801091d7ee34be3f9a
lbzqwe <120498304@qq.com>
2017-01-16 17:30:09 +0800

    sa

--- site/WebApp/settings/base.py
+++ site/WebApp/settings/base.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'WebApp.apps.content'
+    'apps.content'
 )
 
 MIDDLEWARE_CLASSES = (
